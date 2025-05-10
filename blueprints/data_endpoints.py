14d1b7188ae0c1cfb072a2c9b263fc329211fa71
Marcus C. Allen, Ph.D. <marcusCallen24@gmail.com>
2025-05-10 12:26:34 -0400

    updated endpoints to work with new frontend

--- blueprints/data_endpoints.py
+++ blueprints/data_endpoints.py
@@ -1,25 +1,32 @@
 from flask import Blueprint, jsonify, Response
-from datetime import datetime
+from datetime import date
+from dateutil.relativedelta import relativedelta
 import yfinance as yf
 import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)  # load environment variables
 
 data_blueprint = Blueprint("data", __name__, url_prefix="/data")
 
-current_time = datetime.now()
-START_TIME = datetime(month=1, day=1, year=current_time.year)
-END_TIME = current_time
-SYMBOL = "AAPL"
+# TODO: Apply financial time correction
+current_date = date.today()
+START_DATE = date(month=1, day=1, year=current_date.year)
+END_DATE = current_date + relativedelta(days=1)
+
+SYMBOL = os.getenv("SYMBOL")
 
 
 @data_blueprint.route(
     "/ticker",
-    defaults={"symbol": SYMBOL, "start_time": START_TIME, "end_time": END_TIME},
+    defaults={"symbol": SYMBOL, "start_date": START_DATE, "end_date": END_DATE},
 )
-@data_blueprint.route("/ticker/<string:symbol>/range/<start_time>/<end_time>")
-def get_stock_prices(symbol: str, start_time: str, end_time: str) -> Response:
+@data_blueprint.route("/ticker/<string:symbol>/range/<start_date>/<end_date>")
+def get_stock_prices(symbol: str, start_date: str, end_date: str) -> Response:
     df = (
         yf.Ticker(symbol)
-        .history(start=start_time, end=end_time, interval="1h")
+        .history(start=start_date, end=end_date, interval="1d")
         .reset_index()  # pop out datetime index
         .to_json(orient="records", date_format="iso")
     )  # make each row a json object
--- blueprints/prediction_endpoints.py
+++ blueprints/prediction_endpoints.py
@@ -1,23 +1,30 @@
 # standard imports
 from flask import Blueprint, jsonify, Response, current_app
 import numpy as np
+import yfinance as yf
 from dateutil.relativedelta import relativedelta
 from hopsworks.project import Project
 from hsfs.feature_store import FeatureStore
 from datetime import date
 import pandas as pd
+import os
+from dotenv import load_dotenv
 
 # local imports
 from mlops import load_model
 from myfeatures.dates import financial_date_correction
 
+load_dotenv(override=True)  # load environment variables
+SYMBOL = os.getenv("SYMBOL")
+START_DATE = date.today()
 
-pred_blueprint = Blueprint("pred", __name__)
+
+pred_blueprint = Blueprint("pred", __name__, url_prefix="/prediction")
 
 # TODO: Make these vars global between here and the training pipeline
 
 
-def _format_prediction_times(query_date: date, n_preds: int) -> list[date]:
+def _format_prediction_times(query_date: date, n_preds: int) -> list[str]:
     """Format time stamps for predictions relative to the query date from user
 
     Args:
@@ -25,7 +32,7 @@ def _format_prediction_times(query_date: date, n_preds: int) -> list[date]:
         n_preds (int): number of predictions to ahead of the query date
 
     Returns:
-        list[date]: list of prediction timestamps
+        list[str]: list of prediction timestamps
     """
 
     # TODO: Find a way to use a accumulate type of function to simplify this
@@ -40,10 +47,26 @@ def _format_prediction_times(query_date: date, n_preds: int) -> list[date]:
                 pred_dates[idx - 1] + relativedelta(days=1), direction="forward"
             )
 
-    return pred_dates
+    return [
+        dt.isoformat() for dt in pred_dates
+    ]  # needs to be isoformat for front end to read
 
 
-def format_predictions(predictions: np.ndarray, query_date: date) -> Response:
+def _prepend_latest_stock(
+    pred_df: pd.DataFrame, symbol: str, start_date: date
+) -> pd.DataFrame:
+    data_df = (
+        yf.Ticker(symbol)
+        .history(start=start_date, interval="1d")
+        .reset_index()  # pop out datetime index
+        .assign(Date=lambda x: [dt.strftime("%Y-%m-%d") for dt in x["Date"]])
+        .loc[:, ["Close", "Date"]]
+    )
+
+    return pd.concat([data_df, pred_df], ignore_index=True, axis=0)
+
+
+def format_predictions(predictions: np.ndarray, query_date: date) -> pd.DataFrame:
     """format predictions from the trained to be sent to the front-end
 
     Args:
@@ -59,13 +82,16 @@ def format_predictions(predictions: np.ndarray, query_date: date) -> Response:
     n_preds = predictions.shape[0]
 
     pred_dates = _format_prediction_times(query_date, n_preds)
-    df = pd.DataFrame({"predictions": predictions, "date": pred_dates})
-    return jsonify(df.to_dict(orient="records"))
+    return pd.DataFrame({"Close": predictions, "Date": pred_dates})
 
 
-@pred_blueprint.route("/predict")
-def predict() -> Response:
-
+@pred_blueprint.route(
+    "/ticker",
+    defaults={"symbol": SYMBOL, "start_date": START_DATE},
+)
+@pred_blueprint.route("ticker/<string:symbol>/start-date/<start_date>")
+def predict(symbol: str, start_date: str) -> Response:
+    # TODO: select model for given stock and query features based on time
     hopsworks_project: Project = current_app.config["HOPSWORKS_PROJECT"]
     env_name = current_app.config["ENV_NAME"]
     fs: FeatureStore = hopsworks_project.get_feature_store(
@@ -87,4 +113,8 @@ def predict() -> Response:
     model = load_model()
     pred: np.ndarray = model.predict(X, batch_size=X.shape[0])
 
-    return format_predictions(pred, query_date)
+    pred_df = format_predictions(pred, query_date)
+
+    return jsonify(
+        _prepend_latest_stock(pred_df, symbol, start_date).to_dict(orient="records")
+    )
